51a410624a111e7f457c3fb45a469eeed5a413c8
WI-Hyun-joong <wjh4877@naver.com>
2019-02-20 09:19:07 +0900

    build first

---
+++ ReadMe.md
@@ -0,0 +1,4 @@
+... creator commision image sample tile ...
+
+커미션 신청해서 받은 이미지들 언젠간 자랑하려고 하다가 저작권 관련해서 쉽게 이미지 샘플 만들려고 만든 모듈
+
---
+++ imageHandler.js
@@ -0,0 +1,26 @@
+
+/**
+ * init function : handler, event add
+ */
+function init() {
+
+
+}
+
+/**
+ * get element input text 
+ */
+function getTextData() {
+    var elementData = {}
+
+
+    return elementData;
+}
+
+/**
+ * 
+ */
+function getImageHandler() {
+    var imageCanvasInfo = {}
+    
+}
