f41c6e09eb018c391301399f820e54056c1cfd41
IngIeoAndSpare <wjh4877@naver.com>
2019-02-26 13:44:09 +0900

    add style and function define

--- imageHandler.js
+++ imageHandler.js
@@ -18,9 +18,44 @@ function getTextData() {
 }
 
 /**
- * 
+ * filter button eventHandler
+ */
+function filterHandler() {
+
+}
+
+/**
+ * image button eventHandler
  */
 function getImageHandler() {
     var imageCanvasInfo = {}
     
+}
+
+/**
+ * return element object and set event
+ */
+function setEventHandler() {
+    let buttonElement = {
+        fileButton : document.getElementById('loadButton'),
+        filterButton : document.getElementById('filterButton')
+    }
+    let canvasElement = {
+        loadCanvas : document.getElementById('loadCanvas'),
+        filterCanvas : document.getElementById('filterCanvas')
+    }
+
+    let textElement = {
+        commant : document.getElementById('imageCommant'),
+        autor : document.getElementById('imageAutor')
+    }
+    
+    buttonElement.fileButton.addEventListener(getImageHandler);
+    buttonElement.filterButton.addEventListener(filterHandler);
+
+    return {
+        buttonElement,
+        canvasElement,
+        textElement
+    };
 }
