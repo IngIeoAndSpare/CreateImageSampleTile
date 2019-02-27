cdea331ad530bf6b2db48e821c2d82489559b6a7
IngIeoAndSpare <wjh4877@naver.com>
2019-02-27 17:35:13 +0900

    gray scale add

--- imageHandler.js
+++ imageHandler.js
@@ -28,35 +28,61 @@ function filterHandler() {
  * @param {*} file file url
  */
 function getImageSrc(file) {
-    let fileReader = new FileReader();
-
-    //check image file
-    if(!file.type.startsWith('image/')) {
-        alert('not image!');
-        return;
-    }
-    
-    fileReader.onload = () => {
-        return fileReader.result;
-    }
-    fileReader.readAsDataURL(file);
+    return new Promise((resolve, reject) => {
+        let fileReader = new FileReader();
+        //check image file
+        if(!file.type.startsWith('image/')) {
+            alert('not image!');
+            reject(null);
+        }
+        
+        fileReader.onload = () => {
+            let image = new Image();
+            image.src = fileReader.result;
+            image.onload = () => {
+                resolve(image);
+            }
+        }
+        fileReader.readAsDataURL(file);
+    });
 }
 
 /**
  * draw image into canvas
  * @param {element} canvasElement draw image canvas target
- * @param {base64} src image src
+ * @param {image} image image src
  */
-function drawImage(canvasElement, src) {
+function drawImage(canvasElement, image) {
+    let context = canvasElement.getContext('2d');
+    canvasElement.width = image.width;
+    canvasElement.height = image.height;
 
+    context.putImageData(image, 0, 0);
 }
+
+/**
+ * return gray pixel data
+ * @param {pixles} pixles pixel object data 
+ */
+function getGrayScale(pixles) {
+    return pixles.r * 0.2126 + pixles.g * 0.7152 + pixles.b * 0.0722;
+}
+
 
 /**
  * image button eventHandler
  */
 function imageButtonHandler(event) {
-    let loadImageSrc = fileReader(event.target.files[0]);
-    
+    const thisApp = this;
+    getImageSrc(event.target.files[0])
+    .then(result => {
+        if(result) {
+            
+        } else {
+            //not image
+            return;
+        }
+    });
 }
 
 
