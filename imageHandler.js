c5d9551a71262e8a42329dbf9f642fd108960053
IngIeoAndSpare <wjh4877@naver.com>
2019-02-27 10:46:16 +0900

    add handler

--- imageHandler.js
+++ imageHandler.js
@@ -3,6 +3,8 @@
  * init function : handler, event add
  */
 function init() {
+    // element set, eventhandler set
+    let elementSet = setEventHandler();
 
 
 }
@@ -10,11 +12,8 @@ function init() {
 /**
  * get element input text 
  */
-function getTextData() {
-    var elementData = {}
-
-
-    return elementData;
+function getTextData(element) {
+    return element.value;
 }
 
 /**
@@ -25,12 +24,41 @@ function filterHandler() {
 }
 
 /**
+ * return load image src
+ * @param {*} file file url
+ */
+function getImageSrc(file) {
+    let fileReader = new FileReader();
+
+    //check image file
+    if(!file.type.startsWith('image/')) {
+        alert('not image!');
+        return;
+    }
+    
+    fileReader.onload = () => {
+        return fileReader.result;
+    }
+    fileReader.readAsDataURL(file);
+}
+
+/**
+ * draw image into canvas
+ * @param {element} canvasElement draw image canvas target
+ * @param {base64} src image src
+ */
+function drawImage(canvasElement, src) {
+
+}
+
+/**
  * image button eventHandler
  */
-function getImageHandler() {
-    var imageCanvasInfo = {}
+function imageButtonHandler(event) {
+    let loadImageSrc = fileReader(event.target.files[0]);
     
 }
+
 
 /**
  * return element object and set event
@@ -50,7 +78,7 @@ function setEventHandler() {
         autor : document.getElementById('imageAutor')
     }
     
-    buttonElement.fileButton.addEventListener(getImageHandler);
+    buttonElement.fileButton.addEventListener(imageButtonHandler);
     buttonElement.filterButton.addEventListener(filterHandler);
 
     return {
